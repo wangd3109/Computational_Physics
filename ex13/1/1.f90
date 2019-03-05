ff72f4031a6e85f56c8bcec23ba8464bcd63065f
Duo Wang <wangd3109@gmail.com>
2019-03-05 22:01:11 +0100

    计算pi

---
+++ ex13/1/1.f90
@@ -0,0 +1,21 @@
+program ex13
+        implicit none
+        real ::x,y,ratio,totaln,cont
+        integer :: i
+
+        call random_seed()
+        do i=1,100000
+        totaln=totaln+1
+        call random_number(x)
+        call random_number(y)
+        print*,x,y
+        if (x**2+y**2 .lt. 1) then
+                cont=cont+1
+        end if
+
+        end do
+        ratio=4*(cont/totaln)
+        print*,ratio
+
+end program ex13
+
