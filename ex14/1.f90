cb0ac499cc3e439d00632efa9a35d44cd08419a7
Duo Wang <wangd3109@gmail.com>
2019-03-05 22:08:12 +0100

    取1-10之间的任一个随机数，并取整

--- ex14/1.f90
+++ ex14/1.f90
@@ -1,8 +1,9 @@
 program main
       implicit none
-      real :: dx,x(10),h,j,b
+      real :: dx,x(10),h,j,b,y
       integer ::i,s1,s2,summation1,summation2,k
       real,external :: exchange,efield
+
 
       dx=1
       j=1
@@ -10,10 +11,13 @@ program main
 
       do i=1,10
       x(i)=1
-      print*,x(i)
       end do
 
-      print*,x(1),x(3)
+
+      call random_seed()
+      call random_number(y)
+      y=y*10+1
+      print*,y,int(y)
 
       do i=1,10
       if (i .ne. 10) then
@@ -25,16 +29,15 @@ program main
       end if
       summation1=summation1+exchange(s1,s2)
       end do
-      print*, summation1
+!      print*, summation1
 
       do i=1,10
       s1=x(i)
       summation2=summation2+efield(s1)
       end do
-      print*,summation2
+!      print*,summation2
 
       h=-j*summation1-b*summation2
-      print*,"h",h
 
 
 end program main
