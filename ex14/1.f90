48fac84c39e56d745e4a3fd37e15f09fa8bc2391
Duo Wang <wangd3109@gmail.com>
2019-03-09 22:09:45 +0100

    have corrected the ratio part, deleted the kb, Boltzmann constant

--- ex14/1.f90
+++ ex14/1.f90
@@ -1,13 +1,10 @@
 program main
       implicit none
-      real :: dx,x(10),h,j,b,y,summation1,summation2,h1,h2,tmp,t,m
-      real, parameter :: k=1.3806488E+23
+      real(8) :: dx,x(10),h,j,b,y,summation1,summation2,h1,h2,tmp,t,m,p,a
       integer ::i,s1,s2,cont
       real,external :: exchange,efield,r
       
-!      print*, "type the temperature that you want (in K):"
-!      read*,t 
-      t=1000
+      t=1
       j=1.
       b=0
       cont=0
@@ -21,41 +18,39 @@ program main
       call random_number(y)
       y=y*10+1
       x(int(y))=-1
-      end do                            ! now we have x(10
+      end do                            ! now we have the lattice x(10)
 
       call hamil(j,b,x,h1)
-      print*,h1
+      print*,"Hamiltonian_initial:",h1,"x",x
 
       10 continue
       m=0
       call random_number(y)
       y=y*10+1
 
-      x(int(y))=-1*x(int(y))
+      x(int(y))=-1*x(int(y))                    !随机翻转某一位置的自旋
 
       call hamil(j,b,x,h2)
 
-      if (r(h1,h2) .gt. 1) then
-              !print*,"energy difference:-","  possibility:",r(h1,h2),"Hamiltonian:",h2
+      if (h2 .le. h1) then
               cont=cont+1
               call mag(x,m)
-              print*,"-  ","time step:",cont,"hamiltonian:",h2,"magnetization:",m
+              print*,"-  ","energy decrease:",h1-h2,h1,h2,r(h1,h2,t)
               h1=h2
-              goto 10
       else 
               call random_number(tmp)
-              if (tmp .lt. r(h1,h2)) then
+              if (tmp .lt. r(h1,h2,t)) then
                       cont=cont+1
                       call mag(x,m)
-                      !print*,"energy difference:+",tmp,"with the possibility:",r(h1,h2),"Hamiltonian:",h2
-                      print*,"+  ","time step:",cont,"Hamiltonian:",h2,"magnetization:",m
+                      print*, "+  ","energy decrease:",h1-h2,h1,h2,r(h1,h2,t),tmp
                       h1=h2
               else
-                      h1=h1
+                      print*,h2,tmp
+                      x(int(y))=-1*x(int(y))
               end if
-
-              goto 10
       end if
+
+      if (cont .lt. 50) goto 10
 
 end program main
 
@@ -71,15 +66,15 @@ real function efield(a)
         efield=a
 end function
 
-real function r(h1,h2)
+real function r(h1,h2,t)
         implicit none
-        real :: h1,h2,k,t
-        r=exp((h1-h2)/(k*t))
+        real(8) :: h1,h2,t
+        r=exp((h1-h2)/t)
 end function
 
 subroutine hamil(j,b,x,h)
         implicit none
-        real :: x(10),h,j,b,summation1,summation2
+        real(8) :: x(10),h,j,b,summation1,summation2
         integer :: i,s1,s2
         real,external :: exchange, efield
 
@@ -112,7 +107,7 @@ end subroutine hamil
 
 subroutine mag(x,m)
         implicit none
-        real :: x(10),m
+        real(8) :: x(10),m
         integer :: i
 
         do i=1,10
