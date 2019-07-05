f6f7b97c17beda9ad10ec8e95a7909f8565f0812
Duo Wang <wangd3109@gmail.com>
2019-07-05 09:29:46 +0200

    data for different sizes

---
+++ final_project/project3/8/10000steps/Average.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+for i in `seq 5 1 60`
+do
+	  e=$(awk NR==5000,NR==10000'{sum+=$4} END {print "Averaged_Hamiltonian:", sum/(10000-5000)}' t$i)
+	  m=$(awk NR==5000,NR==10000'{sum+=$6} END {print "Averaged_Magnetization:", sum/(10000-5000)}' t$i)
+#	  let t=$i/10.
+	  echo $i $e $m >> avg_32
+done
+
+
+awk 'NR==1{tmp=$3}NR>1{print $0,"\t",$3-tmp;tmp=$3}' avg_32 > avg_32new
+awk 'NR==1{tmp=$5}NR>1{print $0,"\t",$5-tmp;tmp=$5}' avg_32new > avg_32new2
+
+mv avg_32new2 avg_32new
